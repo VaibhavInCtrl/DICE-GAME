59d72a64b7ae3ba865a2939bdef8456f7912b2a0
VaibhavInCtrl <72706715+VaibhavInCtrl@users.noreply.github.com>
2021-04-28 17:32:13 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,60 @@
+var player1 = Math.floor(Math.random()*6)+1;
+var player2 = Math.floor(Math.random()*6)+1;
+console.log(player1,player2);
+function winner(){
+    
+    if (player1 > player2){
+        document.querySelector(".heading").innerText="Player 1 Wins !!! Refresh Again"
+    }
+    else if (player2 > player1){
+        document.querySelector(".heading").innerText="Player 2 Wins !!! Refresh Again"
+    }
+    else {
+        document.querySelector(".heading").innerText="Draw !!! Refresh Again"
+    }
+
+}
+winner();
+
+function imgChanger1(){
+    if (player1 == "1"){
+        document.querySelector("#image1").src = "Images/dice1.png"
+    }
+    else if (player1 == "2"){
+        document.querySelector("#image1").src = "Images/dice2.png"
+    }
+    else if (player1 == "3"){
+        document.querySelector("#image1").src = "Images/dice3.png"
+    }
+    else if (player1 == "4"){
+        document.querySelector("#image1").src = "Images/dice4.png"
+    }
+    else if (player1 == "5"){
+        document.querySelector("#image1").src = "Images/dice5.png"
+    }
+    else if (player1 == "6"){
+        document.querySelector("#image1").src = "Images/dice6.png"
+    }
+}
+imgChanger1();
+function imgChanger2(){
+    if (player2 == "1"){
+        document.querySelector("#image2").src = "Images/dice1.png"
+    }
+    else if (player2 == "2"){
+        document.querySelector("#image2").src = "Images/dice2.png"
+    }
+    else if (player2 == "3"){
+        document.querySelector("#image2").src = "Images/dice3.png"
+    }
+    else if (player2 == "4"){
+        document.querySelector("#image2").src = "Images/dice4.png"
+    }
+    else if (player2 == "5"){
+        document.querySelector("#image2").src = "Images/dice5.png"
+    }
+    else if (player2 == "6"){
+        document.querySelector("#image2").src = "Images/dice6.png"
+    }
+}
+imgChanger2();
